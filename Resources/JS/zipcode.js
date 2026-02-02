a875f198fc6bc27a93fdfe1f9096b0dfc13617d2
jakepeskin <jakepeskin@gmail.com>
2026-02-02 15:02:53 -0800

    countyfixback

--- Resources/JS/zipcode.js
+++ Resources/JS/zipcode.js
@@ -3,199 +3,199 @@ function zipcode() {
     var link;
     if (test == '95301'){
         link = 'atwatervillage.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91301'){
         link = 'agoura.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
      } else if (test == '91377'){
         link = 'agoura.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90077'){
         link = 'belair.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91307'){
         link = 'bellcanyon.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90210' || test == '90211' || test=="90212"){
         link = 'beverlyhills.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90036'){
         link = 'fairfax.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90049'){
         link = 'burbank.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90068'){
         link = 'beachwoodcanyon.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90034'){
         link = 'castleheights.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91302' || test == '91301'){
         link = 'calabasas.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90035' || test == '90019'){
         link = 'carthaycircle.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90067'){
         link = 'centurycity.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90064'){
         link = 'cheviothills.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90012'){
         link = 'chinatown.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90066' || test == '90230' || test == '90233' || test == '90292' || test == '90232'){
         link = 'culvercity.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90012' || test == '90017'){
         link = 'downtown.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90013'){
         link = 'artsdistrict.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90071'){
         link = 'bunkerhill.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90041' || test == '90042' || test == '90065'){
         link = 'eaglerock.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90027' || test == '90029'){
         link = 'easthollywood.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90026'){
         link = 'echopark.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91316'){
         link = 'encino.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91202' || test == '91207' || test == '91208'){
         link = 'glendale.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90004' || test == '90005' || test == '90010' || test == "90020"){
         link = 'hancockpark.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90042'){
         link = 'highlandpark.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90028' || test == '90038' || test == '90046'){
         link = 'hollywood.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90068' || test == '90069'){
         link = 'hollywoodhills.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90027'){
         link = 'hollywoodhillswest.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90302' || test == '90307'){
         link = 'inglewood.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90010' || test == '90005' || test == '90006'){
         link = 'koreatown.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90046'){
         link = 'laurelcanyon.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90004'){
         link = 'larchmontvillage.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90027'){
         link = 'losfeliz.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90265' || test == '90264' || test == '90263'){
         link = 'malibu.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90266' || test == '90267'){
         link = 'manhattanbeach.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90049'){
         link = 'mandevillecanyon.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90066'){
         link = 'marvista.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90292'){
         link = 'marinadelrey.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90029'){
         link = 'melrosehill.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90016'){
         link = 'midcity.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91601' || test == '91606'){
         link = 'northollywood.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90272'){
         link = 'pacificpalisades.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90034'){
         link = 'palms.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90274' || test == '90275'){
         link = 'palosverdes.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91101' || test == '91102' || test == '91103' || test == '91104' || test == '91105' || test == '91106' || test == '91182' || test == '91184' || test == '91185' || test == '91188'){
         link = 'pasadena.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90035'){
         link = 'picorobertson.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90094'){
         link = 'playavista.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90293'){
         link = 'playadelrey.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91108' || test == '91118'){
         link = 'sanmarino.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90026' || test == '90039'){
         link = 'silverlake.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90030' || test == '90031'){
         link = 'southpasadena.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90402' || test == '90405' || test == '90291' || test == '90401' || test == '90403' || test == '90404'){
         link = 'santamonica.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90025' || test == '90264'){
         link = 'sawtelle.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91604'){
         link = 'studiocity.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91403'){
         link = 'shermanoaks.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90069'){
         link = 'sunsetplaza.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91356'){
         link = 'tarzana.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91602'){
         link = 'tolucalake.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90290' || test == '91364'){
         link = 'topangacanyon.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91608'){
         link = 'universalstudios.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90291'){
         link = 'venice.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91401' || test == '91606'){
         link = 'valleyglen.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '91367'){
         link = 'woodlandhills.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else if (test == '90046' || test =='90048'){
         link = 'westhollywood.html';
-        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/lacounty/" + link);
+        window.location.assign("https://www.rosesagencyhomecare.com/areas/neighborhoods/" + link);
     } else{
         link = 'areas/directory.html';
         window.location.assign("https://www.rosesagencyhomecare.com/" + link); 
